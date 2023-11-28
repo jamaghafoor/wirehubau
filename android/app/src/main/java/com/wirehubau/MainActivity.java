a684da31e71808599a27980db2bdf391c905dec5
Abdul Ghafoor <jamghafoor1122@gmail.com>
2023-11-28 15:21:41 +0500

    removed splash changes

--- android/app/src/main/java/com/wirehubau/MainActivity.java
+++ android/app/src/main/java/com/wirehubau/MainActivity.java
@@ -1,6 +1,5 @@
 package com.wirehubau;
 
-import org.devio.rn.splashscreen.SplashScreen; // import this
 import android.os.Bundle; // import this
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -8,13 +7,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-
-   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
