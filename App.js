a09dc4e832ec75cd4d0fe69ea3b5f69f750a3337
Abdul Ghafoor <jamghafoor1122@gmail.com>
2023-11-29 20:38:30 +0500

    Added functionality to handle back button functionality

--- App.js
+++ App.js
@@ -2,26 +2,23 @@
 /* eslint-disable quotes */
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {View, Platform, StyleSheet, StatusBar} from 'react-native';
-import {LogLevel, OneSignal} from 'react-native-onesignal';
+import { View, Platform, StyleSheet, StatusBar, BackHandler, Alert } from 'react-native';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 import SplashScreen from 'react-native-splash-screen';
 import AnimatedLoader from 'react-native-animated-loader';
-import {WebView} from 'react-native-webview';
+import { WebView } from 'react-native-webview';
 
 StatusBar.setBackgroundColor('#fff');
-export default function App() {
+export default function App({ navigation }) {
+  const WEBVIEW_REF = React.useRef(null)
   const [visible, setVisible] = React.useState(true);
+  const [canGoBack, setCanGoBack] = React.useState(false);
 
   OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
   // OneSignal Initialization
   OneSignal.initialize('b7b5efad-787e-4158-8da3-8af9ffb27f1f');
-
-  // requestPermission will show the native iOS or Android notification permission prompt.
-  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
   OneSignal.Notifications.requestPermission(true);
-
-  // Method for listening for notification clicks
   OneSignal.Notifications.addEventListener('click', event => {
     console.log('OneSignal: notification clicked:', event);
   });
@@ -31,7 +28,7 @@ export default function App() {
       <View style={styles.loaderView}>
         <AnimatedLoader
           visible={visible}
-          overlayColor="rgba(255,255,255,0.75)"
+          overlayColor="rgba(255,255,255, 0)"
           source={require('./assets/Loader.json')}
           animationStyle={styles.lottie}
           speed={1}
@@ -45,15 +42,54 @@ export default function App() {
       SplashScreen.hide();
     }, 1000);
   }, []);
+
+  const setupState = event => {
+    setCanGoBack(event?.canGoBack);
+  };
+
+  React.useEffect(() => {
+    const goBack = () => {
+      if (canGoBack === false) {
+        Alert.alert(
+          'Exit App',
+          'Do you want to exit app?',
+          [
+            {text: 'No', onPress: () => console.log('No'), style: 'cancel'},
+            {text: 'Yes', onPress: () => BackHandler?.exitApp()},
+          ],
+          {cancelable: false},
+        );
+      }
+      WEBVIEW_REF?.current?.goBack();
+      return true;
+    };
+
+    BackHandler?.addEventListener('hardwareBackPress', () => goBack());
+
+    return () =>
+      BackHandler?.removeEventListener('hardwareBackPress', () => goBack());
+  }, [canGoBack]);
+
   return (
     <View style={styles.container}>
       <WebView
+        ref={WEBVIEW_REF}
         originWhitelist={['*']}
-        source={{uri: 'https://wirehub.com.au/wp-login.php'}}
+        source={{ uri: 'https://wirehub.com.au/wp-login.php' }}
         renderLoading={LoadingIndicatorView}
         startInLoadingState={true}
-        // onLoadStart={() => setVisible(true)}
         onLoadEnd={() => setVisible(false)}
+        // cacheEnabled={web.cacheEnabled}
+        automaticallyAdjustContentInsets={false}
+        domStorageEnabled={true}
+        cacheEnabled={true}
+        cacheMode={"LOAD_CACHE_ELSE_NETWORK"}
+        allowsInlineMediaPlayback={true}
+        allowsBackForwardNavigationGestures
+        sharedCookiesEnabled={true}
+        thirdPartyCookiesEnabled={true}
+        pullToRefreshEnabled={true}
+        onNavigationStateChange={e => setupState(e)}
       />
     </View>
   );
